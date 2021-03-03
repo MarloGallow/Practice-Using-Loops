3f3fb47ca0d452702992e4900e553ee87de3a467
Marlo Gallow <mgallow226@noscihigh.org>
2021-03-03 01:17:04 +0000

    finished

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 /**
  * IN CLASS EXERCISE: LOOPS
  */
+var myArray = [0,1,2,3,4,5]
+var myObj = {
+  first: 1,
+  second: 2,
+  third: 3
 
+}
 /** 
  * Given an input Array, loop forward over the Array and print its values 
  * using console.log().
  */
 function printArrayValues(array) {
   // YOUR CODE BELOW HERE //
-  
-  
+  for (let a = 0; a <= array.length -1; a++) {
+  console.log(array[a])
+  }
   // YOUR CODE ABOVE HERE //
 }
-
+printArrayValues(myArray)
 
 
 
@@ -26,11 +33,13 @@ function printArrayValues(array) {
  */
 function printArrayValuesInReverse(array) {
   // YOUR CODE BELOW HERE //
-
+for (let c = array.length -1; c >= 0; c--) {
+  console.log(array[c])
+}
   
   // YOUR CODE ABOVE HERE //
 }
-
+printArrayValuesInReverse(myArray)
 
 
 
@@ -41,11 +50,15 @@ function printArrayValuesInReverse(array) {
  */
 function getObjectKeys(object) {
   // YOUR CODE BELOW HERE //
-     
-  
+     let emptyArr = []
+  for(var key in myObjectParam){
+    emptyArr.push(key)
+    }
+    console.log(emptyArr)
+
   // YOUR CODE ABOVE HERE //
 }
-
+getObjectKeys(myObj)
 
 
 
@@ -57,10 +70,13 @@ function getObjectKeys(object) {
 function printObjectKeys(object) {
   // YOUR CODE BELOW HERE //
   
+  for (var key in object) {
+    console.log(key)
+  }
 
   // YOUR CODE ABOVE HERE //
 }
-
+printObjectKeys(myObj)
 
 
 
@@ -70,11 +86,12 @@ function printObjectKeys(object) {
  */
 function getObjectValues(object) {
   // YOUR CODE BELOW HERE //
-  
-  
+  for (var key in object) {   
+console.log(object[key])
+  }
   // YOUR CODE ABOVE HERE //
 }
-
+printObjectvalues(myObj)
 
 
 
@@ -87,8 +104,10 @@ function getObjectValues(object) {
  */
 function printObjectValues(object) {
   // YOUR CODE BELOW HERE //
-
+for (var key in object ) {
+  console.log(object[key])
+  }
   
   // YOUR CODE ABOVE HERE //
 }
-
+printObjectValues(myObj)
